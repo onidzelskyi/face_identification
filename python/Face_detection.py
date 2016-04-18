12f6a63c4834bfdca2d7368348638e18e8749825
Oleksii <onid@softserveinc.com>
2016-04-18 19:04:29 +0300

    Added google viison API demo

---
+++ python/Face_detection.py
@@ -0,0 +1,86 @@
+"""Google vision API Face Detection sample."""
+
+import os
+import global
+import base64
+import httplib2
+
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+from PIL import Image, ImageDraw
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/oleksii/Downloads/Google Cloud Vision-9785cb814b4e.json"
+
+DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+
+
+def get_vision_service():
+    credentials = GoogleCredentials.get_application_default()
+    return discovery.build('vision', 'v1', credentials=credentials,
+                           discoveryServiceUrl=DISCOVERY_URL)
+
+def detect_face(face_file, max_results=10):
+    """Uses the Vision API to detect faces in the given file.
+
+    Args:
+        face_file: A file-like object containing an image with faces.
+
+    Returns:
+        An array of dicts with information about the faces in the picture.
+    """
+    image_content = face_file.read()
+    batch_request = [{
+        'image': {
+            'content': base64.b64encode(image_content).decode('UTF-8')
+            },
+        'features': [{
+            'type': 'FACE_DETECTION',
+            'maxResults': max_results,
+            }]
+        }]
+
+    service = get_vision_service()
+    request = service.images().annotate(body={
+        'requests': batch_request,
+        })
+    response = request.execute()
+
+    return response['responses'][0]['faceAnnotations']
+
+def highlight_faces(image, faces, output_filename):
+    """Draws a polygon around the faces, then saves to output_filename.
+
+    Args:
+      image: a file containing the image with the faces.
+      faces: a list of faces found in the file. This should be in the format
+          returned by the Vision API.
+      output_filename: the name of the image file to be created, where the faces
+          have polygons drawn around them.
+    """
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+
+    for face in faces:
+        box = [(v.get('x', 0.0), v.get('y', 0.0)) for v in face['fdBoundingPoly']['vertices']]
+        draw.line(box + [box[0]], width=5, fill='#00ff00')
+
+    del draw
+    im.save(output_filename)
+
+def main(input_filename, output_filename):
+    with open(input_filename, 'rb') as image:
+        faces = detect_face(image)
+        print('Found %s face%s' % (len(faces), '' if len(faces) == 1 else 's'))
+
+        print('Writing to file %s' % output_filename)
+        # Reset the file pointer, so we can read the file again
+        image.seek(0)
+        highlight_faces(image, faces, output_filename)
+        
+
+if __name__ == "__main__":
+    main("/media/sf_host_dev/github/face_identification/test/g7.jpg", \ 
+         "/home/oleksii/Downloads/out.jpg")        
+
